141eaa76c64024a41b51acedc95f0c44bec32cbd
Quique Fdez Guerra <quique@ckgrafico.com>
2021-04-10 13:17:35 +0200

    Add logo

--- components/Header.vue
+++ components/Header.vue
@@ -1,37 +1,41 @@
 <template>
-  <header class="max-w-5xl mx-auto py-8 flex">
-    <nav>
-      <ul>
-        <li>
-          <nuxt-link class="text-teal-600 hover:underline" to="/">
-            Home
+  <header class="relative bg-white">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6">
+      <div
+        class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10"
+      >
+        <div class="flex justify-start lg:w-0 lg:flex-1">
+          <nuxt-link to="/">
+            <img
+              class="h-8 w-auto sm:h-10"
+              src="@/assets/images/logo_landscape.png"
+              alt="Logo"
+            />
           </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link class="text-teal-600 hover:underline" to="/articles">
-            Blog
+        </div>
+        <nav class="hidden md:flex space-x-10">
+          <nuxt-link
+            to="articles"
+            class="text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Articles
           </nuxt-link>
-        </li>
-      </ul>
-    </nav>
-    <div class="flex-1">
-      <a href="/" class="block w-56 mx-auto">
-        <img src="http://a.storyblok.com/f/42016/1096x313/0353bf6654/logo2.png" />
-      </a>
+        </nav>
+        <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+          <a
+            href="#"
+            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Sign in
+          </a>
+          <a
+            href="#"
+            class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+          >
+            Sign up
+          </a>
+        </div>
+      </div>
     </div>
-    <nav>
-      <ul>
-        <li>
-          <nuxt-link class="text-teal-600 hover:underline" to="/">
-            English
-          </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link class="text-teal-600 hover:underline" to="/es">
-            Spanish
-          </nuxt-link>
-        </li>
-      </ul>
-    </nav>
   </header>
 </template>
