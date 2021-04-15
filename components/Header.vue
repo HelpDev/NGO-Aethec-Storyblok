25279bb69975a9e4e96df59179a01d70f6be7e64
Quique Fdez Guerra <quique@ckgrafico.com>
2021-04-15 18:12:50 +0200

    Add socialicon component

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,7 @@
 <template>
   <header class="relative bg-primary-500">
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
-      <div
-        class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10"
-      >
+      <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
         <div class="flex justify-start lg:w-0 lg:flex-1">
           <nuxt-link :to="localePath('/')">
             <img
@@ -73,7 +71,6 @@
 import TranslateIcon from 'heroicons/outline/translate.svg?inline';
 
 export default {
-  name: 'Header',
   components: {
     TranslateIcon
   },
