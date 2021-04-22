29be78c8bcff4dfff39f7612c6781677714d7097
Quique Fdez Guerra <quique@ckgrafico.com>
2021-04-22 21:47:21 +0200

    update styles

--- components/page/PageHero.vue
+++ components/page/PageHero.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="w-full h-4/5">
-    <div>
-      <h2>{{blok.title}}</h2>
+    <div class="bg-primary-500 text-white w-60 text-2xl">
+      <h2>{{ blok.title }}</h2>
     </div>
     <div
       v-if="blok.image"
       class="h-full"
-      :style="{backgroundImage: `url(${blok.image.filename})`}"
+      :style="{ backgroundImage: `url(${blok.image.filename})` }"
     ></div>
   </div>
 </template>
