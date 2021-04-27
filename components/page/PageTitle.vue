41e438c2fa6b06307c0ef455b592856d4dad7518
Quique Fdez Guerra <quique@ckgrafico.com>
2021-04-27 21:02:20 +0200

    Add projects

--- components/page/PageTitle.vue
+++ components/page/PageTitle.vue
@@ -1,10 +1,12 @@
- <template>
+<template>
   <div class="flex flex-col text-center w-full mb-20">
-    <h1 class="text-2xl font-bold title-font mb-4 text-gray-900">{{ blok.text }}</h1>
-    <h2
-      v-if="blok.subtitle"
-      class="lg:w-2/3 mx-auto leading-relaxed text-base"
-    >{{ blok.subtitle }}</h2>
+    <div class="py-8 w-full text-center text-5xl bg-opacity-90 z-20">
+      <h1>{{ blok.text }}</h1>
+    </div>
+
+    <h2 v-if="blok.subtitle" class="lg:w-2/3 mx-auto leading-relaxed text-base">
+      {{ blok.subtitle }}
+    </h2>
   </div>
 </template>
 
